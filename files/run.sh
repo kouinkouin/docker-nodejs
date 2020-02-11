064305e2e5864e2ce1da3fd28b50059b1e810c36
Raphaël Pommier <rpommier@thefabrik.io>
2020-02-11 11:18:00 +0100

    feat: keep env variables into sudo

--- files/run.sh
+++ files/run.sh
@@ -3,5 +3,5 @@ set -e
 
 [ $(id -u www-data) -ne $UID -o $(id -g www-data) -ne $GID -o "/var/www" != $HOME ] && groupmod -g $GID www-data && usermod -u $UID -d $HOME www-data
 
-exec sudo -u www-data $@
+exec sudo -E -u www-data $@
 
